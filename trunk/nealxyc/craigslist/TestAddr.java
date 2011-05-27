6f1e738877fc3f2e2fa4f7909452411edc186909
nealxyc@gmail.com <nealxyc@gmail.com@b7cebd2a-ef5f-39be-182d-4e7604809099>
2011-05-27 07:55:25 +0000

    added address fetching

---
+++ trunk/nealxyc/craigslist/TestAddr.java
@@ -0,0 +1,38 @@
+package nealxyc.craigslist;
+
+import java.util.ArrayList;
+
+public class TestAddr {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		LoggerInit.init();
+		
+		//String link = "http://sfbay.craigslist.org/sfc/apa/";
+		String link = "http://sfbay.craigslist.org/search/apa/sfc?query=&srchType=A&minAsk=&maxAsk=1100&bedrooms=";
+		ArrayList<Item> list = Crawler.fetchItemFromPage(link, "");
+		
+		System.out.println("==========");
+		System.out.println(list.size() + " results.");
+		int count = 3 ;
+		for(Item it : list){
+			
+			it.setAddress(Crawler.fetchAddr(it.getLink()));
+			System.out.println(it.getTitle());
+			System.out.println(it.getAddress());
+			System.out.println("==========");
+			count -- ;
+			if(count <= 0){
+				break ;
+			}
+		}
+		
+		
+		
+
+	}
+
+}
